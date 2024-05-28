afc48693d009ff7c514da8d20758210e79a823e5
shri-hari27 <shrihari2027@gmail.com>
2024-05-28 09:29:38 +0530

    modified firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAO3EwKSepAqc8elE8JGz3-rpHdhMpT-9I",
-  authDomain: "slack-clone0-fa11d.firebaseapp.com",
-  projectId: "slack-clone0-fa11d",
-  storageBucket: "slack-clone0-fa11d.appspot.com",
-  messagingSenderId: "866579881361",
-  appId: "1:866579881361:web:d3de4e890b1b5eff8e8e30",
-  measurementId: "G-6KJPYJ0SQ0",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
